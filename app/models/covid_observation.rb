d781b53c42e686f8606d1507ae33e7970a00acc9
itchan <christianbaman4@gmail.com>
2023-02-06 23:19:20 +0800

    Create covid_observation model and migration

--- app/models/covid_observation.rb
+++ app/models/covid_observation.rb
@@ -1,2 +1,5 @@
 class CovidObservation < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_obsercation_date, against: :observation_date
 end
--- db/migrate/20230206122805_create_covid_observations.rb
+++ db/migrate/20230206122805_create_covid_observations.rb
@@ -1,7 +1,13 @@
 class CreateCovidObservations < ActiveRecord::Migration[7.0]
   def change
     create_table :covid_observations do |t|
-
+      t.datetime :observation_date
+      t.string :state
+      t.string :country
+      t.datetime :last_update
+      t.integer :confirmed
+      t.integer :deaths
+      t.integer :recovered
       t.timestamps
     end
   end
